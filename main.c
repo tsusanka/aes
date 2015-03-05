3b3884e8085c530c7178b4ad8ea3c7fde16e254f
Tomas Susanka <tsusanka@gmail.com>
2015-03-05 15:41:52 +0100

    Debug: show bits function

--- main.c
+++ main.c
@@ -1,8 +1,26 @@
+/**
+ * MI-BHW ukol 1 - AES
+ *
+ * Tomas Susanka & Peter Poljak
+ *
+ **/
+
+#include <stdio.h>
+
+ 
+void showbits(unsigned int x)
+{
+	int i; 
+	for (i = (sizeof(int) * 8) - 1; i >= 0; i--)
+	{
+		(x&(1<<i)) ? putchar('1') : putchar('0');
+	}
+
+	printf("\n");
+}
 
 int main(int argc, char** argv)
 {
-	
-	// todo
 
 	return 0;
 }
