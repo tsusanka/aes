afec8de3de89e8df1475486db99b08cace1a59a2
Tomas Susanka <tsusanka@gmail.com>
2015-03-05 16:43:20 +0100

    input scanning

--- main.c
+++ main.c
@@ -7,8 +7,10 @@
 
 #include <stdio.h>
 
- 
-void printBin(char x)
+#define BLOCK_SIZE_ROW_LENGTH 4
+
+
+void printBin(unsigned char x)
 {
 	int i; 
 	for (i = (sizeof(char) * 8) - 1; i >= 0; i--)
@@ -19,7 +21,7 @@ void printBin(char x)
 	printf("\n");
 }
 
-void printHex(char x)
+void printHex(unsigned char x)
 {
 	printf("0x%x", x);
 	printf("\n");
@@ -27,6 +29,22 @@ void printHex(char x)
 
 int main(int argc, char** argv)
 {
+	unsigned char block[BLOCK_SIZE_ROW_LENGTH][BLOCK_SIZE_ROW_LENGTH];
+	int i, y = 0;
+	unsigned char input;
+
+	printf("Enter %d bytes in hex seperated by newline: \n", BLOCK_SIZE_ROW_LENGTH * BLOCK_SIZE_ROW_LENGTH);
+
+	for (i = 0; i < BLOCK_SIZE_ROW_LENGTH; ++i)
+	{
+		for (y = 0; y < BLOCK_SIZE_ROW_LENGTH; ++y)
+		{
+			scanf("%x", &input);
+			block[y][i] = input;
+			printf("Read: %x \n", block[i][y]);
+		}
+	}
+
 
 	return 0;
 }
