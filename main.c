c60d52baebc892c7081dc3bd91efc18b36cabba3
Tomas Susanka <tsusanka@gmail.com>
2015-03-05 18:56:04 +0100

    shift rows sublayer

--- main.c
+++ main.c
@@ -48,30 +48,57 @@ uint8_t sboxify(uint8_t num)
 	return sboxTable[high_nibble * SBOX_TABLE_ROWS + low_nibble];
 }
 
-void substitutionLayer(uint8_t blockA[][BLOCK_SIZE_ROW_LENGTH])
+void substitutionLayer(uint8_t block[][BLOCK_SIZE_ROW_LENGTH])
 {
 	int i, y = 0;
 	for (i = 0; i < BLOCK_SIZE_ROW_LENGTH; i++)
 	{
 		for (y = 0; y < BLOCK_SIZE_ROW_LENGTH; y++)
 		{
-			blockA[i][y] = sboxify(blockA[i][y]);
+			block[i][y] = sboxify(block[i][y]);
 		}
 	}
 }
 
+void shiftRowLeft(uint8_t block[][BLOCK_SIZE_ROW_LENGTH], int i)
+{
+	int y = 0;
+	uint8_t temp;
+
+	temp = block[i][0];
+	for (y = 0; y < BLOCK_SIZE_ROW_LENGTH; y++)
+	{
+		block[i][y] = block[i][(y+1) % 4] ;
+	} 
+
+	block[i][y-1] = temp;
+}
+
+void shiftRowsSublayer(uint8_t block[][BLOCK_SIZE_ROW_LENGTH])
+{
+	shiftRowLeft(block,1);
+	shiftRowLeft(block,2);
+	shiftRowLeft(block,2);
+	shiftRowLeft(block,3);
+	shiftRowLeft(block,3);
+	shiftRowLeft(block,3);
+}
+
 int main(int argc, char** argv)
 {
-	uint8_t blockA[BLOCK_SIZE_ROW_LENGTH][BLOCK_SIZE_ROW_LENGTH] = {
+	uint8_t block[BLOCK_SIZE_ROW_LENGTH][BLOCK_SIZE_ROW_LENGTH] = {
 		{0x00, 0x44, 0x88, 0xCC},
 		{0x11, 0x55, 0x99, 0xDD},
 		{0x22, 0x66, 0xAA, 0xEE},
 		{0x33, 0x77, 0xBB, 0xFF},
 	};
 
-	substitutionLayer(blockA);
+	substitutionLayer(block);
+	printBlock(block);
 
-	printBlock(blockA);
+	shiftRowsSublayer(block);
+	
+	printBlock(block);
 
 	return 0;
 }
