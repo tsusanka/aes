9c063f3d6e0217854e7a5854550c5a64dc55dda4
Tomas Susanka <tsusanka@gmail.com>
2015-03-05 18:10:34 +0100

    sbox

--- main.c
+++ main.c
@@ -12,7 +12,6 @@
 #define BLOCK_SIZE_ROW_LENGTH 4
 
 
-
 void printBin(uint8_t x)
 {
 	int i; 
@@ -41,16 +40,37 @@ void printBlock(uint8_t block[][BLOCK_SIZE_ROW_LENGTH])
 	printf("----------------\n");
 }
 
+uint8_t sboxify(uint8_t num)
+{
+	uint8_t high_nibble = num>>4 & 0x0F;
+	uint8_t low_nibble = num & 0x0F;
+
+	return sboxTable[high_nibble * SBOX_TABLE_ROWS + low_nibble];
+}
+
 int main(int argc, char** argv)
 {
-	uint8_t block[BLOCK_SIZE_ROW_LENGTH][BLOCK_SIZE_ROW_LENGTH] = {
+	int i, y = 0;
+
+	uint8_t blockA[BLOCK_SIZE_ROW_LENGTH][BLOCK_SIZE_ROW_LENGTH] = {
 		{0x00, 0x44, 0x88, 0xCC},
 		{0x11, 0x55, 0x99, 0xDD},
 		{0x22, 0x66, 0xAA, 0xEE},
 		{0x33, 0x77, 0xBB, 0xFF},
 	};
+	
+	uint8_t blockB[BLOCK_SIZE_ROW_LENGTH][BLOCK_SIZE_ROW_LENGTH];
 
-	printBlock(block);
+	for (i = 0; i < BLOCK_SIZE_ROW_LENGTH; i++)
+	{
+		for (y = 0; y < BLOCK_SIZE_ROW_LENGTH; y++)
+		{
+			blockB[i][y] = sboxify(blockA[i][y]);
+		}
+	}
+
+	printBlock(blockA);
+	printBlock(blockB);
 
 	return 0;
 }
